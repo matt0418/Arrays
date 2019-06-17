a8249c55361518400e016ab640e7143d0d7b9a09
matt0418 <mfeldma7@gmail.com>
2019-06-17 15:53:48 -0400

    completed read function

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -92,8 +92,12 @@ void resize_array(Array *arr) {
 char *arr_read(Array *arr, int index) {
 
   // Throw an error if the index is greater or equal to than the current count
+  if (arr->count >= index) {
+    perror("Index is out of range");
+  }
 
   // Otherwise, return the element at the given index
+  return arr->elements[index];
 }
 
 
